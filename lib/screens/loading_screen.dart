c28625d3eff863296a957a4367c76277478901d7
Purnendu <mailtopv2299@gmail.com>
2021-05-21 23:50:52 +0530

    get location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,13 +7,29 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    // LocationPermission permission = await Geolocator.checkPermission();
+    // print(permission);
+    // LocationPermission permission1 = await Geolocator.requestPermission();
+    // print(permission1);
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);  //also take permission!!!
+    print(position);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
+        child: ElevatedButton/*in place of RaisedButton*/(
           onPressed: () {
-            //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
@@ -20,3 +37,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
     );
   }
 }
+
+
+//Latitude: 25.62162162162162, Longitude: 85.16734872717178
